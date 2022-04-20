c6f74e3676bbbc10a4974b44c9ff0000d4f0c1b5
crazyro15 <crazyro15@hotmail.com>
2022-04-20 12:38:15 -0300

    Primeros 5 ejercicios

---
+++ ejercicios js/ejercicios.js
@@ -0,0 +1,22 @@
+//Ej1
+alert("Hello World!");
+
+//Ej2
+document.write("Hello World!<br>");
+document.write("-----------------------------<br>");
+
+//Ej3
+document.write(3+5);
+document.write("<br>-----------------------------<br>");
+
+//Ej4
+let nombre = prompt("Ingrese su nombre");
+document.write("Hola " + nombre);
+document.write("<br>-----------------------------<br>");
+
+//Ej5
+let num1 = parseInt(prompt("Ingrese el primer número"));
+let num2 = parseInt(prompt("Ingrese el segundo número"));
+document.write("El resultado de la suma es: " + (num1 + num2));
+
+
